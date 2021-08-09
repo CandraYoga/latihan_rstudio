97114f7cc6c5a4859db6c3da94fd7b0837c2393c
CandraYoga <211201906185@mhs.dinus.ac.id>
2021-08-09 14:19:34 +0700

    second commit

--- halo.r
+++ halo.r
@@ -4,4 +4,4 @@ teks2 = "Anda pasti akan senang dengan kehebatannya"
 teks1
 teks2
 
-list1 = list("Sate Kambing","Gado gado","Pecel lele")
+list1 = list("Sate Kambing","Gado gado","Pecel lele","Ayam bakar")
